5b0bf21bbf50f5c9150db31a447fada676de4113
Lucas Dejard <erlucomlpg@gmail.com>
2022-07-04 15:41:42 -0300

    Testes com Behave

--- features/api.feature
+++ features/api.feature
@@ -1,14 +1,20 @@
 # language:pt
 
-Funcionalidade:
+Funcionalidade: Saber as tarefas que tenho a realizar
 
   """
   Seja eu uma pessoa interessante
   Quero saber quais tarefas tenho
   """
   Cenário: No primeiro uso do sistema, não existem tarefas registradas
-    Dado que exista uma tarefa
-      |nome|descrição|estado|
-      |toma pinga|pq é bom|false|
     Quando verificar minhas tarefas em "/tasks"
     Então não devo ter tarefas para fazer
+
+  Cenário: ver tasks registradas
+    Dado que exista uma tarefa
+      |nome|descrição|estado|
+      |toma pinga|pq é bom|False|
+    Quando verificar minhas tarefas em "/tasks"
+    Então devo ter a seguinte tarefa para fazer
+      |nome|descrição|estado|
+      |toma pinga|pq é bom|False|
--- features/steps/api.py
+++ features/steps/api.py
@@ -1,5 +1,7 @@
 from httpx import get, post
 from behave import then, when, given
+from json import load
+from ast import literal_eval
 
 url_base = 'http://localhost:5000/todos/api/tasks'
 
@@ -7,9 +9,10 @@ url_base = 'http://localhost:5000/todos/api/tasks'
 def inserir_tarefa(context):
     feature_table = context.table[0]
     tarefa = {}
-    tarefa['title'] = feature_table['nome']
+    tarefa['tittle'] = feature_table['nome']
     tarefa['description'] = feature_table['descrição']
-    tarefa['state'] = feature_table['estado']
+    tarefa['done'] = feature_table['estado']
+
     assert post(url_base, json=tarefa).status_code == 201
 
 @when('verificar minhas tarefas em "{endpoint}"')
@@ -17,14 +20,17 @@ def pegando_minhas_tarefas(context, endpoint):
     context.request = get(url_base)
 
 @then('não devo ter tarefas para fazer')
-def checando_se_não_tem_tarefa(context):
-    context.request.json() == []
+def checando_se_nao_tem_tarefa(context):
+    assert context.request.json() == []
 
-@then('devo ter a sguinte tarefa para fazer')
+@then('devo ter a seguinte tarefa para fazer')
 def checar_se_tarefa_esta_para_ser_feita(context):
     feature_table = context.table[0]
     tarefa = {}
-    tarefa['title'] = feature_table['nome']
+    tarefa['tittle'] = feature_table['nome']
     tarefa['description'] = feature_table['descrição']
-    tarefa['state'] = feature_table['estado']
-    assert context.requestjson() == []
+    tarefa['done'] = literal_eval(feature_table['estado'])
+    response = context.request.json()
+    del response[0]['id']
+
+    assert tarefa in response, f'{response} {tarefa}'
